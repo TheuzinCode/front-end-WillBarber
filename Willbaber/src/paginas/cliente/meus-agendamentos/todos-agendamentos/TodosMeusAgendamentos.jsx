69aba7d5c8bd1e52e7fb3420eb795b019437e4c7
Matheus Santos <matheus.gssantos01@gmail.com>
2026-05-27 12:33:57 -0300

    cliente cancelando corte

--- Willbaber/src/paginas/cliente/meus-agendamentos/todos-agendamentos/TodosMeusAgendamentos.jsx
+++ Willbaber/src/paginas/cliente/meus-agendamentos/todos-agendamentos/TodosMeusAgendamentos.jsx
@@ -1,18 +1,49 @@
 import "./TodosMeusAgendamentos.css"
+import Swal from "sweetalert2";
 
-const TodosMeusAgendamentos = ({todosAgendamentos}) => {
-  
+const TodosMeusAgendamentos = ({ todosAgendamentos }) => {
+
   async function CancelarAgendamento(agendamentoId) {
 
+    const body = "CANCELADO";
     try {
-        const resp = await fetch(
-          ``
-        )
+      const resp = await fetch(
+        `http://localhost:8080/willbarber/agendamento/agendamentos/${agendamentoId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(body)
+        }
+      )
+
+      if (!resp.ok) {
+        const erro = await resp.text();
+        Swal.fire({
+          icon: "error",
+          title: "Erro",
+          text: erro
+        });
+
+        return;
+      }
+
+      Swal.fire({
+        icon: "success",
+        title: "Cancelado com sucesso!",
+        text: "Seu horário foi cancelado com sucesso.",
+        confirmButtonColor: "#C9A646",
+        background: "#111111",
+        color: "#FFFFFF"
+      });
+
+
     } catch (error) {
       console.error("Erro ao cancelar agendamento:", error);
     }
   }
-  
+
   return (
     <>
       <div className="lista-agendamentos">
@@ -35,11 +66,11 @@ const TodosMeusAgendamentos = ({todosAgendamentos}) => {
                     ).toLocaleString("pt-BR")}
                   </span>
                   <span className={
-                    agendamento.statusAgendamento === "AGENDADO" 
-                    ? "status-pendente-todos" 
-                    : agendamento.statusAgendamento === "CANCELADO"
-                      ? "status-cancelado-todos"
-                      : "status-confirmado-todos"
+                    agendamento.statusAgendamento === "AGENDADO"
+                      ? "status-pendente-todos"
+                      : agendamento.statusAgendamento === "CANCELADO"
+                        ? "status-cancelado-todos"
+                        : "status-confirmado-todos"
                   }>
                     {agendamento.statusAgendamento}
                   </span>
@@ -49,8 +80,8 @@ const TodosMeusAgendamentos = ({todosAgendamentos}) => {
             <div className="lado-direito-agendamento-todos">
               <h1>R$ {agendamento.valorServico},00</h1>
               <div className="acoes-agendamento-todos">
-                <button className="botao-cancelar-todos" 
-                onClick={() => CancelarAgendamento(agendamento.id)}>
+                <button className="botao-cancelar-todos"
+                  onClick={() => CancelarAgendamento(agendamento.id)}>
                   Cancelar
                 </button>
               </div>
